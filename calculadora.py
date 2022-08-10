f28fb665e2612af1c29682f339dc7a0bb3c53ac5
julianaac <juliana.1419981@gmail.com>
2022-08-10 17:30:33 -0500

    #4 O usuário deverá realizar novo calculo sem executar a aplicação novamente

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,46 @@
-operation = input ('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
+def calculate():
+    operation = input ('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    ''')
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+        # Add again () function to calculate() function
+        again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
 ''')
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    if calc_again.upper() == 'Y':
+        calculate()
 
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    
+    elif calc_again.upper() == 'N':
+        print('See you later.')
 
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+    else:
+        again()
 
-else:
-    print('You have not typed a valid operator, please run the program again.')
+calculate()
