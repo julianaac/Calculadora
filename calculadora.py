3ff3b19aa0b99b26050708fb1bef5eb1b490dedd
julianaac <juliana.1419981@gmail.com>
2022-08-10 16:02:22 -0500

    #3 Permitir que o usuário selecione a operação que deseja

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,26 @@
-number_1 = int(input('Primeiro numero: '))
-number_2 = int(input('Segundo numero: '))
-print(f'{number_1} + {number_2} =', number_1 + number_2)
+operation = input ('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
 
-#Subtraction 
-print(f'{number_1} - {number_2} =', number_1 - number_2)
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
 
-#Multiplication
-print(f'{number_1} * {number_2} =', number_1 * number_2)
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
 
-#Division
-print(f'{number_1} / {number_2} =', number_1 / number_2)
+else:
+    print('You have not typed a valid operator, please run the program again.')
